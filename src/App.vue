062739069bbcd920a3db0e22b9d5cb79df7d1b9d
devoradorDeBytes <49806802+devoradorDeBytes@users.noreply.github.com>
2020-06-03 12:04:25 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <div class="notes-section">
+      <div class="columns">
+        <div class="column has-text-centered">
+          <strong>Notes</strong>
+          <div v-for="note in notes" v-bind:key="note.id" class="notes">{{note}}</div>
+        </div>
+        <div class="column has-text-centered">
+          <strong>Timestamp</strong>
+          <div
+            v-for="timestamp in timestamps"
+            v-bind:key="timestamp.id"
+            class="timestamps"
+          >{{ timestamp }}</div>
+        </div>
+      </div>
+      <input-component @add-note="addNote" :placeholder="placeholder"></input-component>
+    </div>
+  </div>
+</template>
+
+<script>
+import inputComponent from "@/components/inputComponent";
+import "bulma/css/bulma.css";
+export default {
+  name: "HelloWorld",
+  components: {
+    inputComponent
+  },
+  data() {
+    return {
+      notes: [],
+      timestamps: [],
+      placeholder: "Enter a note"
+    };
+  },
+  methods: {
+    addNote(event) {
+      this.notes.push(event.note);
+      this.timestamps.push(event.timestamp);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/inputComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="inputComponent">
+    <input
+      :placeholder="placeholder"
+      v-model="input"
+      v-on:keyup.enter="monitorEnterKey"
+      class="input is-small"
+      type="text"
+    >
+    <button v-on:click="monitorEnterKey">Add 1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "inputComponent",
+  props: ["placeholder"],
+  data() {
+    return {
+      input: ""
+    };
+  },
+  methods: {
+    monitorEnterKey() {
+      this.$emit("add-note", {
+        note: this.input,
+        timestamp: new Date().toLocaleString()
+      });
+      //alert("le has dado al enter");
+      this.input = "";
+    }
+  }
+};
+</script>
